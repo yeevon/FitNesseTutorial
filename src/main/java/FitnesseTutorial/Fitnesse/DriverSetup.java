e0bfb0c603bfb4c297534bd3ff14437026607102
Jose de Lima <delimajm@gmail.com>
2024-10-18 13:30:41 -0500

    Added Readme instructsions for MFA, Testing PDF, setting up Admin users

--- src/main/java/FitnesseTutorial/Fitnesse/DriverSetup.java
+++ src/main/java/FitnesseTutorial/Fitnesse/DriverSetup.java
@@ -4,11 +4,13 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.WebDriver;
 import org.jetbrains.annotations.NotNull;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 import java.time.Duration;
 
 public class DriverSetup {
     public WebDriver setup() {
-        WebDriver driver = new ChromeDriver();
+        WebDriver driver = new FirefoxDriver();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
         return  driver;
     }
